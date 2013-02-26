d2ce3c58aad875cc7c6d01fb513e8bcd85c213d0
Chris Bednarski <banzaimonkey@gmail.com>
2013-02-25 18:18:56 -0800

    Added support for spark.yml configuration

---
+++ src/cbednarski/Spark/Config.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace cbednarski\Spark;
+
+use Symfony\Component\Yaml\Yaml;
+
+class Config
+{
+    private $data;
+
+    public static function loadFile($path)
+    {
+        $data = Yaml::parse($path);
+        return new static($data);
+    }
+
+    public function __construct($data = null)
+    {
+        $this->data = array_merge(static::defaultConfig(), $data);
+    }
+
+    public static function defaultConfig()
+    {
+        return array(
+            'source' => 'src/',
+            'target' => 'build/target/',
+            'cache' => 'build/cache/',
+            'locale' => 'locale/',
+            'localize' => 'all'
+        );
+    }
+
+    public function getData()
+    {
+
+        return $this->data;
+    }
+
+    public function __get($variable)
+    {
+        if (array_key_exists($variable, $this->data)) {
+            return $this->data[$variable];
+        } else {
+            return null;
+        }
+    }
+}
---
+++ tests/cbednarski/Spark/ConfigTest.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once(__DIR__ . '/../../../vendor/autoload.php');
+
+use cbednarski\Spark\Config;
+use Symfony\Component\Yaml\Yaml;
+
+class ConfigTest extends PHPUnit_Framework_TestCase
+{
+    private $standard_config;
+
+    public function setup()
+    {
+        # __DIR__ can't be used in the variable definition so we need to use setup()
+        $this->standard_config = __DIR__ . '/../../assets/spark.yml';
+    }
+
+    public function testStandardConfig()
+    {
+        $config = Config::loadFile($this->standard_config);
+        $this->assertEquals($config->getData(), Yaml::parse($this->standard_config));
+    }
+
+    public function testPartialConfig()
+    {
+        $config_file = __DIR__ . '/../../assets/spark_incomplete.yml';
+        $config = Config::loadFile($config_file);
+        $this->assertEquals($config->getData(), Yaml::parse($this->standard_config));
+
+        # These configs should be filled in even though they aren't in the .yml
+        $this->assertEquals('build/cache/', $config->cache);
+        $this->assertEquals('locale/', $config->locale);
+    }
+
+    public function testCustomConfig()
+    {
+        $config_file = __DIR__ . '/../../assets/spark_custom.yml';
+        $config = Config::loadFile($config_file);
+
+        # These configs are overridden completely
+        $expected = array(
+            'source' => 'src/',
+            'target' => 'my/custom/build/target/',
+            'cache' => 'build/cache/', # This one isn't specified in .yml though
+            'locale' => 'po_files/',
+            'localize' => array(
+                'en_US' => 'en',
+                'fr_FR' => 'fr',
+                'de_DE' => 'de',
+            )
+        );
+
+        $this->assertEquals($expected, $config->getData());
+    }
+}
