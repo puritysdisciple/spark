69c938387e7b34870492cad4124d5ea0fa27004d
Chris Bednarski <banzaimonkey@gmail.com>
2013-03-06 01:46:18 -0800

    Reformat

--- tests/cbednarski/Spark/ProjectTest.php
+++ tests/cbednarski/Spark/ProjectTest.php
@@ -18,18 +18,18 @@ class ProjectTest extends PHPUnit_Framework_TestCase
 
         $this->assertTrue(file_exists($path . '/.gitignore'));
         $this->assertTrue(file_exists($path . '/spark.yml'));
-        $this->assertTrue(file_exists($path . '/'. $config->assets));
-        $this->assertTrue(file_exists($path . '/'. $config->assets . '/css/main.css'));
-        $this->assertTrue(file_exists($path . '/'. $config->layouts));
-        $this->assertTrue(file_exists($path . '/'. $config->layouts . '/layout.html.twig'));
-        $this->assertTrue(file_exists($path . '/'. $config->pages));
-        $this->assertTrue(file_exists($path . '/'. $config->pages . '/index.html.twig'));
-        $this->assertTrue(file_exists($path . '/'. $config->target));
-        $this->assertTrue(file_exists($path . '/'. $config->locale . '/en_US/LC_MESSAGES'));
-        $this->assertTrue(file_exists($path . '/'. $config->locale . '/en_US/LC_MESSAGES/messages.po'));
-        $this->assertTrue(file_exists($path . '/'. $config->locale . '/fr_FR/LC_MESSAGES'));
-        $this->assertTrue(file_exists($path . '/'. $config->locale . '/fr_FR/LC_MESSAGES/messages.po'));
-        $this->assertTrue(file_exists($path . '/'. $config->plugins . '/sample_plugin.php'));
+        $this->assertTrue(file_exists($path . '/' . $config->assets));
+        $this->assertTrue(file_exists($path . '/' . $config->assets . '/css/main.css'));
+        $this->assertTrue(file_exists($path . '/' . $config->layouts));
+        $this->assertTrue(file_exists($path . '/' . $config->layouts . '/layout.html.twig'));
+        $this->assertTrue(file_exists($path . '/' . $config->pages));
+        $this->assertTrue(file_exists($path . '/' . $config->pages . '/index.html.twig'));
+        $this->assertTrue(file_exists($path . '/' . $config->target));
+        $this->assertTrue(file_exists($path . '/' . $config->locale . '/en_US/LC_MESSAGES'));
+        $this->assertTrue(file_exists($path . '/' . $config->locale . '/en_US/LC_MESSAGES/messages.po'));
+        $this->assertTrue(file_exists($path . '/' . $config->locale . '/fr_FR/LC_MESSAGES'));
+        $this->assertTrue(file_exists($path . '/' . $config->locale . '/fr_FR/LC_MESSAGES/messages.po'));
+        $this->assertTrue(file_exists($path . '/' . $config->plugins . '/sample_plugin.php'));
 
         FileUtils::recursiveDelete($path);
     }
