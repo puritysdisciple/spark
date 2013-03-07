f3e6fc61c9d405c673dec896358942d94c03ef72
Chris Bednarski <banzaimonkey@gmail.com>
2013-03-07 13:45:26 -0800

    Added build locales task

---
+++ src/cbednarski/Spark/Command/BuildLocales.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace cbednarski\Spark\Command;
+
+use cbednarski\Spark\Config;
+use cbednarski\Spark\Compiler;
+use cbednarski\Spark\FileUtils;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class BuildLocales extends Command
+{
+    protected function configure()
+    {
+        $this->setName('build-locales');
+        $this->setDescription('Build a localized spark project under the specified directory');
+        $this->addArgument(
+            'directory',
+            InputArgument::OPTIONAL,
+            'spark project folder (defaults to the current directory if not specified)'
+        );
+        $this->addOption(
+            'no-clean',
+            null,
+            InputOption::VALUE_NONE,
+            'If set, the build command will not clean the target folder before building'
+        );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $directory = $input->getArgument('directory');
+
+        if (!$directory) {
+            $directory = getcwd();
+        }
+
+        $config = Config::loadFile($directory . '/spark.yml');
+        $compiler = new Compiler($config);
+        $compiler->setOutput($output);
+
+        # Clean out the build target unless no-clean is detected
+        if (!$input->getOption('no-clean')) {
+            FileUtils::recursiveDelete($config->getFullPath('target'));
+        }
+
+        $output->writeln('<info>Building localized project under ' . realpath($directory) . '</info>');
+        $compiler->buildLocales();
+        $output->writeln('<info>Build complete.</info>');
+    }
+}
