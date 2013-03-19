24603254d269e5079070fd94b082863d41a2ebe9
Chris Bednarski <banzaimonkey@gmail.com>
2013-03-18 18:19:53 -0700

    Added to Git class

--- src/cbednarski/Spark/Git.php
+++ src/cbednarski/Spark/Git.php
@@ -1,60 +1,66 @@
 <?php
 
-/**
- * Determine the git version based on `git describe` and semver conventions.
- *
- * Normally we'll pull a version number like:
- *   1.0.4      Tagged commit
- *   1.0.4+306  Untagged commit on top of a tag
- * 
- * If the strict param is true, we will show 'unknown' in the untagged case
- *
- * @param string $path Directory to inspect for git tags
- * @param bool $strict Whether or not we'll allow untagged commits
- * @return string Version number or 'unknown' in a failure case
- */
-function getGitVersion($path, $strict = false)
+namespace cbednarski\Spark;
+
+class Git
 {
-    $temp_path = getcwd();
-    // Git needs the cwd to be inside the repo
-    // proc_open will pick this up automatically
-    chdir($path);
+    /**
+     * Determine the git version based on `git describe` and semver conventions.
+     *
+     * Normally we'll pull a version number like:
+     *   1.0.4      Tagged commit
+     *   1.0.4+306  Untagged commit on top of a tag
+     * 
+     * If the strict param is true, we will show 'unknown' in the untagged case
+     *
+     * @param string $path Directory to inspect for git tags
+     * @param bool $strict Whether or not we'll allow untagged commits
+     * @return string Version number or 'unknown' in a failure case
+     */
+    public static function getGitVersion($path, $strict = false)
+    {
+        $temp_path = getcwd();
+        // Git needs the cwd to be inside the repo
+        // proc_open will pick this up automatically
+        chdir($path);
 
-    $descriptorspec = array(
-        0 => array('pipe', 'r'), // stdin
-        1 => array('pipe', 'w'), // stdout
-        2 => array('pipe', 'w'), // stderr
-    );
+        $descriptorspec = array(
+            0 => array('pipe', 'r'), // stdin
+            1 => array('pipe', 'w'), // stdout
+            2 => array('pipe', 'w'), // stderr
+        );
 
-    $process = proc_open('git describe', $descriptorspec, $pipes);
-    $return_value = null;
+        $process = proc_open('git describe', $descriptorspec, $pipes);
+        $return_value = null;
 
-    if (is_resource($process)) {
-        $raw_version = stream_get_contents($pipes[1]);
-        $errors = stream_get_contents($pipes[2]);
-        $return_value = proc_close($process);
-    }
+        if (is_resource($process)) {
+            $raw_version = stream_get_contents($pipes[1]);
+            $errors = stream_get_contents($pipes[2]);
+            $return_value = proc_close($process);
+        }
 
-    if ($return_value !== 0) {
-        return 'unknown';
-    }
+        if ($return_value !== 0) {
+            return 'unknown';
+        }
 
-    if (preg_match('/^(\d+\.\d+.\d+)(?:\-(\d+)\-[\w\d]+)?$/', $raw_version, $matches) === 1) {
-        if (isset($matches[2])) {
-            if ($strict) {
-                return 'unknown';
+        if (1 === preg_match('/^(\d+\.\d+.\d+)(?:\-(\d+)\-[\w\d]+)?$/', $raw_version, $matches)) {
+            if (isset($matches[2])) {
+                if ($strict) {
+                    return 'unknown';
+                } else {
+                    $version = $matches[1] . '+' . $matches[2];
+                }
             } else {
-                $version = $matches[1] . '+' . $matches[2];
+                $version = $matches[1];
             }
         } else {
-            $version = $matches[1];
+            return 'unknown';
         }
-    } else {
-        return 'unknown';
+
+        // Reset the working directory
+        chdir($temp_path);
+
+        return $version;
     }
 
-    // Reset the working directory
-    chdir($temp_path);
-
-    return $version;
 }
