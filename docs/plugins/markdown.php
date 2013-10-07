ac4da4116a49793daea34830926638d5d7df1725
Chris Bednarski <cbednarski@riotgames.com>
2013-10-07 16:29:14 -0700

    Added spark site for docs, which for now just includes the readme

---
+++ docs/plugins/markdown.php
@@ -0,0 +1,60 @@
+<?php
+
+require_once(__DIR__ . '/../vendor/cbednarski/php-markdown/src/MarkdownParser.php');
+
+class Twig_Node_Markdown extends Twig_Node
+{
+    public function __construct(Twig_NodeInterface $body, $lineno, $tag = 'markdown')
+    {
+        parent::__construct(array('body' => $body), array(), $lineno, $tag);
+    }
+
+    public function compile(Twig_Compiler $compiler)
+    {
+        $content = MarkdownParser::parse($this->getNode('body')->getAttribute('data'));
+
+        $compiler
+            ->write('echo ')
+            ->string($content)
+            ->raw(";\n");
+    }
+}
+
+class Markdown_Set_TokenParser extends Twig_TokenParser
+{
+    public function parse(Twig_Token $token)
+    {
+        $lineno = $token->getLine();
+
+        $this->parser->getStream()->expect(Twig_Token::BLOCK_END_TYPE);
+        $body = $this->parser->subparse(array($this, 'decideMarkdownEnd'), true);
+        $this->parser->getStream()->expect(Twig_Token::BLOCK_END_TYPE);
+
+        return new Twig_Node_Markdown($body, $lineno, $this->getTag());
+    }
+
+    public function decideMarkdownEnd(Twig_Token $token)
+    {
+        return $token->test('endmarkdown');
+    }
+
+    public function getTag()
+    {
+        return 'markdown';
+    }
+}
+
+class Markdown_Extension extends Twig_Extension
+{
+    public function getName()
+    {
+        return 'markdown';
+    }
+
+    public function getTokenParsers()
+    {
+        return array(new Markdown_Set_TokenParser());
+    }
+}
+
+$spark->addTwigExtension(new Markdown_Extension());
